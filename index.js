56bb027c2a16b504fc9329222166ac547ff2c644
James Newell <j.newell@nib.com.au>
2016-06-27 15:34:03 +1000

    fix instrumentation to not instrument test files

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+const extRegex = require('ext-to-regex');
 
 module.exports = tradie => {
+
+  const extensions = tradie.config.scripts.extensions;
+  const scriptRegexp = extRegex(extensions);
+  const testScriptRegexp = extRegex(extensions.map(extension => `.test${extension}`));
 
   console.log('tradie-plugin-coverage');
 
   if (tradie.context === 'test') {
 
+    //configure webpack to
     tradie.on('test.webpack-config', config => {
 
       //configure `isparta-loader`
@@ -14,16 +20,19 @@ module.exports = tradie => {
         babel: {}
       };
 
-      //find and remove the `babel-loader`
       const babelLoader = config.module.loaders.find(spec => spec.loader === 'babel');
-      if (babelLoader) {
-
-        //run `babel-loader` on test files
-        //run `isparta-loader` on source files
-
-        babelLoader.loader = 'isparta';
-
+      if (!babelLoader) {
+        throw new Error('Loader `babel-loader` is not configured.');
       }
+
+      //run `isparta-loader` only on source files
+      config.module.loaders.push(Object.assign({}, babelLoader, {
+        test: path => scriptRegexp.test(path) && !testScriptRegexp.test(path),
+        loader: 'isparta'
+      }));
+
+      //modify `babel-loader` to only run on test files
+      babelLoader.test = testScriptRegexp;
 
     });
 
